3a55228ca9298f5a85edb0c1e3f84224d163365e
deepglugs <deepglugs@gmail.com>
2022-08-02 15:42:10 -0700

    use cropped method to train unet2+; add text encoder trainer

--- imagen.py
+++ imagen.py
@@ -2,17 +2,20 @@ import math
 import numbers
 import os
 import random
+import re
 import time
 from collections import deque
 
 import numpy as np
 import torch
+from torch.utils.data import Dataset
 from torchvision import transforms
 from torchvision.transforms import functional as VTF
 from torchvision.utils import make_grid, save_image
 from PIL import Image
+from tqdm import tqdm
 
-from imagen_pytorch import ImagenTrainer, ElucidatedImagenConfig
+from imagen_pytorch import ImagenTrainer, ElucidatedImagenConfig, ImagenConfig
 from imagen_pytorch import load_imagen_from_checkpoint
 from gan_utils import get_images, get_vocab
 from data_generator import ImageLabelDataset
@@ -60,7 +63,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--source', type=str, default=None, help="image source")
     parser.add_argument('--tags_source', type=str, default=None, help="tag files. will use --source if not specified.")
-    parser.add_argument('--poses', type=str, default=None)
+    parser.add_argument('--cond_images', type=str, default=None)
     parser.add_argument('--tags', type=str, default=None)
     parser.add_argument('--vocab', default=None)
     parser.add_argument('--size', default=256, type=int)
@@ -74,19 +77,26 @@ def main():
     parser.add_argument('--unet_dims', default=128, type=int)
     parser.add_argument('--unet2_dims', default=64, type=int)
     parser.add_argument("--start_size", default=64, type=int)
+    parser.add_argument("--sample_unet", default=None, type=int)
+    parser.add_argument('--device', type=str, default="cuda")
+    parser.add_argument('--text_encoder', type=str, default="t5-large")
+    parser.add_argument("--cond_scale", default=10, type=float, help="sampling conditional scale 0-10.0")
+    parser.add_argument('--no_elu', action='store_true', help="don't use elucidated imagen")
 
     # training
     parser.add_argument('--batch_size', default=8, type=int)
     parser.add_argument('--micro_batch_size', default=8, type=int)
     parser.add_argument('--samples_out', default="samples")
     parser.add_argument('--train', action='store_true')
+    parser.add_argument('--train_encoder', action='store_true')
     parser.add_argument('--shuffle_tags', action='store_true')
     parser.add_argument('--train_unet', type=int, default=1)
     parser.add_argument('--random_drop_tags', type=float, default=0.)
     parser.add_argument('--fp16', action='store_true')
+    parser.add_argument('--bf16', action='store_true')
     parser.add_argument('--workers', type=int, default=8)
     parser.add_argument('--no_text_transform', action='store_true')
-    parser.add_argument('--aug', action='store_true', help="additional image augmentations")
+    parser.add_argument('--start_epoch', default=1, type=int)
 
     args = parser.parse_args()
 
@@ -101,6 +111,13 @@ def main():
     else:
         assert os.path.isfile(args.vocab)
 
+    if args.bf16:
+        # probably (maybe) need to set TORCH_CUDNN_V8_API_ENABLED=1 in environment
+        torch.set_float32_matmul_precision("medium")
+
+    if args.train_encoder:
+        train_encoder(args)
+
     if args.train:
         train(args)
     else:
@@ -112,27 +129,37 @@ def sample(args):
         return
 
     try:
-        imagen = load(args.imagen)
+        imagen = load(args.imagen).to(args.device)
     except:
         print(f"Error loading model: {args.imagen}")
         return
 
+    args.num_unets = len(imagen.unets) - 1
+
+    image_sizes = get_image_sizes(args)
+    print(f"image sizes: {image_sizes}")
+
+    imagen.image_sizes = image_sizes
+
     cond_image = None
 
-    if args.poses is not None and os.path.isfile(args.poses):
+    if args.cond_images is not None and os.path.isfile(args.cond_images):
         tforms = transforms.Compose([PadImage(),
                                      transforms.Resize((args.size, args.size)),
                                      transforms.ToTensor()])
-        cond_image = Image.open(args.poses)
+        cond_image = Image.open(args.cond_images)
         cond_image = tforms(cond_image).to(imagen.device)
         cond_image = cond_image.view(1, *cond_image.shape)
 
     sample_images = imagen.sample(texts=[args.tags],
                                   cond_images=cond_image,
-                                  cond_scale=7.,
-                                  return_pil_images=True)
+                                  cond_scale=args.cond_scale,
+                                  return_pil_images=True,
+                                  stop_at_unet_number=args.sample_unet)
 
-    sample_images[0].save(args.output)
+    final_image = sample_images[-1]
+    final_image.resize((args.size, args.size)).save(args.output)
+
 
 def restore_parts(state_dict_target, state_dict_from):
     for name, param in state_dict_from.items():
@@ -146,6 +173,7 @@ def restore_parts(state_dict_target, state_dict_from):
             print(f"layer {name}({param.size()} different than target: {state_dict_target[name].size()}")
 
     return state_dict_target
+
 
 def save(imagen, path):
     out = {}
@@ -166,6 +194,20 @@ def load(path):
     return imagen
 
 
+def get_image_sizes(args):
+    image_sizes = [args.start_size]
+
+    for i in range(0, args.num_unets):
+        ns = image_sizes[-1] * 4
+        if args.train:
+            ns = ns // 4
+        image_sizes.append(ns)
+
+    image_sizes[-1] = args.size // 4 if args.train else args.size
+
+    return image_sizes
+
+
 def get_imagen(args, unet_dims=None, unet2_dims=None):
 
     if unet_dims is None:
@@ -174,7 +216,7 @@ def get_imagen(args, unet_dims=None, unet2_dims=None):
     if unet2_dims is None:
         unet2_dims = args.unet2_dims
 
-    if args.poses is not None:
+    if args.cond_images is not None:
         cond_images_channels = 3
     else:
         cond_images_channels = 0
@@ -183,11 +225,11 @@ def get_imagen(args, unet_dims=None, unet2_dims=None):
     unet1 = dict(
         dim=unet_dims,
         cond_dim=512,
-        dim_mults=(1, 2, 3, 4),
+        dim_mults=(1, 2, 4, 6),
         cond_images_channels=cond_images_channels,
         num_resnet_blocks=3,
         layer_attns=(False, True, True, True),
-        memory_efficient=False
+        memory_efficient=True
     )
 
     unets = [unet1]
@@ -197,10 +239,10 @@ def get_imagen(args, unet_dims=None, unet2_dims=None):
         unet2 = dict(
             dim=unet2_dims // (i + 1),
             cond_dim=512,
-            dim_mults=(1, 2, 3, 6),
+            dim_mults=(1, 2, 4, 4),
             cond_images_channels=cond_images_channels,
             num_resnet_blocks=(2, 4, 8, 8),
-            layer_attns=(False, False, False, True),
+            layer_attns=(False, False, False, i < 2),
             layer_cross_attns=(False, False, True, True),
             final_conv_kernel_size=1,
             memory_efficient=True
@@ -208,30 +250,39 @@ def get_imagen(args, unet_dims=None, unet2_dims=None):
 
         unets.append(unet2)
 
-    image_sizes = [args.start_size]
-
-    for i in range(0, len(unets)-1):
-        image_sizes.append(image_sizes[-1] * 4)
+    image_sizes = get_image_sizes(args)
 
     print(f"image_sizes={image_sizes}")
 
     sample_steps = [args.sample_steps] * (args.num_unets + 1)
 
-    imagen = ElucidatedImagenConfig(
-        unets=unets,
-        text_encoder_name='t5-large',
-        num_sample_steps=sample_steps,
-        # noise_schedules=["cosine", "cosine"],
-        # pred_objectives=["noise", "x_start"],
-        image_sizes=image_sizes,
-        per_sample_random_aug_noise_level=True,
-        lowres_sample_noise_level=0.3
-    ).create().cuda()
+    if not args.no_elu:
+        imagen = ElucidatedImagenConfig(
+            unets=unets,
+            text_encoder_name=args.text_encoder,
+            num_sample_steps=sample_steps,
+            # noise_schedules=["cosine", "cosine"],
+            # pred_objectives=["noise", "x_start"],
+            image_sizes=image_sizes,
+            per_sample_random_aug_noise_level=True,
+            lowres_sample_noise_level=0.3
+        ).create().to(args.device)
+
+    else:
+        imagen = ImagenConfig(
+            unets=unets,
+            text_encoder_name=args.text_encoder,
+            noise_schedules=["cosine", "cosine"],
+            pred_objectives=["noise", "x_start"],
+            image_sizes=image_sizes,
+            per_sample_random_aug_noise_level=True,
+            lowres_sample_noise_level=0.3
+        ).create().to(args.device)
 
     return imagen
 
 
-def make_training_samples(poses, trainer, args, epoch, step):
+def make_training_samples(poses, trainer, args, epoch, step, epoch_loss):
     sample_texts = ['1girl, red_bikini, bikini, outdoors, pool, brown_hair',
                     '1girl, blue_dress, eyes_closed, blonde_hair',
                     '1boy, black_hair',
@@ -243,15 +294,26 @@ def make_training_samples(poses, trainer, args, epoch, step):
     if poses is not None:
         sample_poses = poses[:disp_size]
 
-
     if poses is not None and sample_poses is None:
         sample_poses = poses[:disp_size]
 
+    # dup the sampler's image sizes temporarily:
+    args.train = False
+    sample_image_sizes = get_image_sizes(args)
+    args.train = True
+
+    train_image_sizes = trainer.imagen.image_sizes
+
+    trainer.imagen.image_sizes = sample_image_sizes
+
     sample_images = trainer.sample(texts=sample_texts,
                                    cond_images=sample_poses,
-                                   cond_scale=7.,
+                                   cond_scale=args.cond_scale,
                                    return_all_unet_outputs=True,
                                    stop_at_unet_number=args.train_unet)
+
+    # restore train image sizes:
+    trainer.imagen.image_sizes = train_image_sizes
 
     final_samples = None
 
@@ -275,7 +337,7 @@ def make_training_samples(poses, trainer, args, epoch, step):
         sample_images = torch.cat([sample_images.cpu(), sample_poses0.cpu()])
 
     grid = make_grid(sample_images, nrow=disp_size, normalize=False, range=(-1, 1))
-    VTF.to_pil_image(grid).save(os.path.join(args.samples_out, f"imagen_{epoch}_{int(step / epoch)}.png"))
+    VTF.to_pil_image(grid).save(os.path.join(args.samples_out, f"imagen_{epoch}_{int(step / epoch)}_loss{round(epoch_loss / step, 5)}.png"))
 
 
 def train(args):
@@ -292,26 +354,31 @@ def train(args):
 
     imgs = get_images(args.source, verify=False)
     txts = get_images(args.tags_source, exts=".txt")
-    # vocab = get_vocab(args.vocab, top=args.vocab_limit)
+    
+    print(f"{len(imgs)} images")
+    print(f"{len(txts)} tags")
 
-    poses = None
+    cond_images = None
     has_poses = False
 
-    if args.poses is not None:
-        poses = get_images(args.poses)
+    if args.cond_images is not None:
+        cond_images = get_images(args.cond_images)
         has_poses = True
+
+    # get non-training sizes for image resizing/cropping
+    args.train = False
+    train_img_size = get_image_sizes(args)[args.train_unet - 1]
 
     tforms = transforms.Compose([
             PadImage(),
-            transforms.Resize((args.size, args.size)),
+            transforms.Resize(train_img_size),
             transforms.ToTensor()])
 
-    if args.aug:
+    if args.train_unet > 1:
         tforms = transforms.Compose([
-            PadImage(),
-            transforms.RandomCrop(args.size),
+            transforms.Resize(args.size),
+            transforms.RandomCrop(train_img_size),
             transforms.RandomHorizontalFlip(),
-            transforms.RandomRotation(degrees=(0, 180)),
             transforms.ToTensor()])
 
 
@@ -339,7 +406,7 @@ def train(args):
         tag_transform = None
 
     data = ImageLabelDataset(imgs, txts, None,
-                             poses=poses,
+                             poses=cond_images,
                              dim=(args.size, args.size),
                              transform=tforms,
                              tag_transform=tag_transform,
@@ -356,56 +423,140 @@ def train(args):
 
     os.makedirs(args.samples_out, exist_ok=True)
 
-    for epoch in range(1, args.epochs + 1):
+    print(f"training on {len(data)} images")
+
+    train_dtype = torch.float16
+
+    if args.bf16:
+        train_dtype = torch.bfloat16
+
+    for epoch in range(args.start_epoch, args.epochs + 1):
         step = 0
-        for data in dl:
+        epoch_loss = 0
+        with tqdm(dl, unit="batches") as tepoch:
+            for data in tepoch:
 
-            if has_poses:
-                images, texts, poses = data
-            else:
-                images, texts = data
-                poses = None
+                if has_poses:
+                    images, texts, cond_images = data
+                else:
+                    images, texts = data
+                    cond_images = None
 
-            step += 1
+                step += 1
 
-            t1 = time.monotonic()
-            losses = []
+                t1 = time.monotonic()
+                losses = []
+                with torch.cuda.amp.autocast(enabled=args.bf16, dtype=train_dtype):
+                    loss = trainer(
+                        images,
+                        cond_images=cond_images,
+                        texts=texts,
+                        unet_number=args.train_unet,
+                        max_batch_size=args.micro_batch_size
+                    )
 
-            loss = trainer(
-                images,
-                cond_images=poses,
-                texts=texts,
-                unet_number=args.train_unet,
-                max_batch_size=args.micro_batch_size
-            )
+                    trainer.update(unet_number=args.train_unet)
 
-            trainer.update(unet_number=args.train_unet)
+                t2 = time.monotonic()
+                rate.append(round(1.0 / (t2 - t1), 2))
 
-            losses.append(loss)
+                epoch_loss += loss
+                tepoch.set_postfix(loss=round(loss, 5), epoch_loss=round(epoch_loss / step, 5))
 
-            t2 = time.monotonic()
-            rate.append(round(1.0 / (t2 - t1), 2))
+                # if step % 10 == 0:
+                #     print("epoch {}/{} step {}/{} loss: {} - {}it/s".format(
+                #           epoch,
+                #           args.epochs,
+                #           step * args.batch_size,
+                #           len(imgs),
+                #           round(np.sum(losses), 5),
+                #           round(np.mean(rate), 2)))
 
-            if step % 10 == 0:
-                print("epoch {}/{} step {}/{} loss: {} - {}it/s".format(
-                      epoch,
-                      args.epochs,
-                      step * args.batch_size,
-                      len(imgs),
-                      round(np.sum(losses), 5),
-                      round(np.mean(rate), 2)))
+                if step % 100 == 0:
+                    make_training_samples(cond_images, trainer, args, epoch,
+                                          step, epoch_loss)
 
-            if step % 100 == 0:
-                make_training_samples(poses, trainer, args, epoch, step)
-
-                if args.imagen is not None:
-                    trainer.save(args.imagen)    
-
+                    if args.imagen is not None:
+                        trainer.save(args.imagen)
         # END OF EPOCH
-        make_training_samples(poses, trainer, args, epoch, step)
+        make_training_samples(cond_images, trainer, args, epoch, step,
+                              epoch_loss)
 
         if args.imagen is not None:
             trainer.save(args.imagen)
+
+
+class LineByLineTextDataset(Dataset):
+    def __init__(self, tokenizer, file_paths: str, block_size=512):
+
+        lines = []
+
+        for file_path in tqdm(file_paths):
+            assert os.path.isfile(file_path)
+            with open(file_path, encoding="utf-8") as f:
+                lines.extend([line for line in f.read().splitlines() if (len(line) > 0 and not line.isspace())])
+
+        if tokenizer is not None:
+            self.examples = tokenizer.batch_encode_plus(lines, add_special_tokens=True, max_length=block_size)["input_ids"]
+        else:
+            self.examples = lines
+
+    def __len__(self):
+        return len(self.examples)
+
+    def __getitem__(self, i):
+        return self.examples[i]
+
+
+def train_encoder(args):
+
+    from transformers import T5ForConditionalGeneration, TrainingArguments, Trainer
+    from transformers import DataCollatorForLanguageModeling
+    from transformers import T5Tokenizer
+
+    pretrained = "t5-small"
+
+    if os.path.exists(args.output):
+        pretrained = args.output
+
+    model = T5ForConditionalGeneration.from_pretrained(pretrained)
+    orig_tokenizer = T5Tokenizer.from_pretrained("t5-small")
+
+    txts = get_images(args.tags_source, exts=".txt")
+
+    # tok_dataset = LineByLineTextDataset(None, txts)
+
+    # tokenizer = orig_tokenizer.train_new_from_iterator(tok_dataset, 52000)
+    tokenizer = orig_tokenizer
+    tokenizer.save_pretrained(args.output)
+
+    tokenizer.pad_token = tokenizer.eos_token
+
+    lm_dataset = LineByLineTextDataset(tokenizer, txts)
+    val_dataset = LineByLineTextDataset(tokenizer, txts[-2:])
+    
+    data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+
+    training_args = TrainingArguments(
+                                      output_dir=args.output,
+                                      evaluation_strategy="epoch",
+                                      learning_rate=2e-5,
+                                      weight_decay=0.01,
+                                      num_train_epochs=args.epochs,
+                                      auto_find_batch_size=True,
+                                      save_strategy="epoch",
+                                      save_total_limit=3
+                                     )
+
+    trainer = Trainer(
+                      model=model,
+                      args=training_args,
+                      train_dataset=lm_dataset,
+                      eval_dataset=val_dataset,
+                      data_collator=data_collator,
+                     )
+
+    trainer.train()
 
 
 if __name__ == "__main__":
